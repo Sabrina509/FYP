aa96cba8e493b8a7b49eb4c266b372f5c19cdb3a
Sabrina Asim <sabrinaasim2000@gmail.com>
2022-01-02 20:28:05 +0500

    sales page finalized

--- src/tutorial/react-router/setup/sales.js
+++ src/tutorial/react-router/setup/sales.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { MenuItems } from './MenuItems'
 import './navbar.css'
+import './sales.css'
 import { Link } from 'react-router-dom'
 
 class Sales extends Component {
@@ -25,6 +26,76 @@ class Sales extends Component {
             <span>Logout</span>
           </button>
         </nav>
+        <div className='enter'>
+          <div className='split'>
+            <div className='leftSide'>
+              <h1>Enter Sales</h1>
+              <form className='form'>
+                <div>
+                  <label className='label' htmlFor='Name'>
+                    {' '}
+                    Name
+                  </label>
+                  <input
+                    className='inp'
+                    type='text'
+                    id='productName'
+                    name='productName'
+                  ></input>
+                </div>
+                <div>
+                  <label className='label1' htmlFor='Quantity'>
+                    {' '}
+                    Quantity
+                  </label>
+                  <input
+                    className='inp'
+                    type='text'
+                    id='quantity'
+                    name='quantity'
+                  ></input>
+                </div>
+                <div>
+                  <button className='AddBtn' type='submit'>
+                    {' '}
+                    <span>ADD</span>{' '}
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className='rightSide'>
+              <div className='App'>
+                <table>
+                  <tr>
+                    <th>Name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                  <tr>
+                    <td>Burger</td>
+                    <td>1</td>
+                    <td>300</td>
+                  </tr>
+                  <tr>
+                    <td>Ice cream</td>
+                    <td>1</td>
+                    <td>100</td>
+                  </tr>
+                  <tr>
+                    <td>Loaded Fries</td>
+                    <td>1</td>
+                    <td>350</td>
+                  </tr>
+                  <tr className='total'>
+                    <td>Total</td>
+                    <td></td>
+                    <td>750</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
