595aa3cc1fbbdeaea767b6f5fcdc97edbc156fb5
Sabrina Asim <sabrinaasim2000@gmail.com>
2022-01-02 16:38:32 +0500

    homepage finalized 1

--- src/tutorial/react-router/setup/home.js
+++ src/tutorial/react-router/setup/home.js
@@ -7,41 +7,30 @@ class Home extends Component {
     return (
       <div className='start'>
         <nav className='NavbarItems'>
-          <h1 className='navbar-logo'>Enterprise Resource Planning</h1>
+          <h1 className='navbar-logo'>Cafe Aylanto</h1>
 
           <ul className='nav-menu'>
             {MenuItems.map((item, index) => {
               return (
                 <li key={index}>
-                  <a className={MenuItems.cName} href={item.url}>
+                  <a className='nav-links' href={item.url}>
                     {item.title}
                   </a>
                 </li>
               )
             })}
           </ul>
-          <button className='logBtn'>Logout</button>
+          <button className='logBtn'>
+            <span>Logout</span>
+          </button>
         </nav>
-        <div className='row'>
-          <div className='bg'></div>
-
-          <div className='para'>
-            <h1>OUR STORY</h1>
-            <p>
-              The Pizza Hut story begins in May 1958. Dan and Frank Carney
-              opened their 550-square foot pizza restaurant in Wichita, Kansas.
-              Dan and Frank, with friends Richard Beemer and John Bender, made
-              the pizza themselves. Frank rolled the dough with a rolling pin.
-              Richard tossed the dough in the air from the pie tin. Dan filled
-              the unbaked crust with sauce. John flipped the pie from the tin
-              during the baking process. Dan Carney recalled, "The people were
-              enchanted with the product, watching us tossing the dough over our
-              heads. They were really enjoying themselves" The Carneys created
-              an innovative, high quality, engaging, informal eating experience
-              in a friendly neighborhood restaurant. Pizza Hut became a popular
-              place with the teenage and college crowd.
-            </p>
-          </div>
+        <div className='textt'>
+          <h1>WELCOME TO ERP SYSTEM</h1>
+        </div>
+        <div className='text2'>
+          <p>
+            <em>An Efficicent Way to Manage your Resources</em>
+          </p>
         </div>
       </div>
     )
